3b216e31ca4d1dc1ca6204fdb4fa1273a158675c
Mauro Alderete <alderete.mauro@gmail.com>
2023-03-25 06:11:39 -0300

    feat: code puppeteer service with puppeteer exception and implement it on session controller with create and delete session

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShamController } from './sham/sham.controller';
 import { PuppeteerService } from './puppeteer-service/puppeteer-service';
+import { SessionController } from './session/session.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ShamController],
+  controllers: [AppController, ShamController, SessionController],
   providers: [AppService, PuppeteerService],
 })
 export class AppModule {}
--- src/puppeteer-service/puppeteer-service.ts
+++ src/puppeteer-service/puppeteer-service.ts
@@ -3,25 +3,37 @@ import * as puppeteer from 'puppeteer';
 
 @Injectable()
 export class PuppeteerService {
-  private browerInstances: Map<string, puppeteer.Browser> = new Map();
+  private browser: puppeteer.Browser;
+  private browerContexts: Map<string, puppeteer.BrowserContext> = new Map();
 
-  async createBrowserInstance(clientId: string): Promise<puppeteer.Browser> {
-    const browser = await puppeteer.launch();
-    this.browerInstances.set(clientId, browser);
-    return browser;
-  }
-
-  async getBrowserInstance(
-    clientId: string,
-  ): Promise<puppeteer.Browser | null> {
-    return this.browerInstances.get(clientId) || null;
-  }
-
-  async closeBrowserInstance(clientId: string): Promise<void> {
-    const browser = await this.getBrowserInstance(clientId);
-    if (browser) {
-      await browser.close();
-      this.browerInstances.delete(clientId);
+  async createBrowserContext(clientId: string): Promise<void> {
+    if (this.browerContexts.has(clientId)) {
+      throw new Error(
+        `Browser instance already exists for client ID: ${clientId}`,
+      );
     }
+
+    if (!this.browser) {
+      this.browser = await puppeteer.launch({ headless: false });
+    }
+
+    const context = await this.browser.createIncognitoBrowserContext();
+    this.browerContexts.set(clientId, context);
+  }
+
+  async getBrowserContext(
+    clientId: string,
+  ): Promise<puppeteer.BrowserContext | null> {
+    return this.browerContexts.get(clientId) || null;
+  }
+
+  async deleteBrowserContext(clientId: string): Promise<void> {
+    const context = this.browerContexts.get(clientId);
+    if (!context) {
+      throw new Error(`Browser instance not found for client ID: ${clientId}`);
+    }
+
+    await context.close();
+    this.browerContexts.delete(clientId);
   }
 }
